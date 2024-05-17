da8beca6fc5d3248ec5a6de441df64f6bea2376e
Yulia <juliakanjuk007@gmail.com>
2024-05-17 10:36:39 +0300

    Виправлення логіки, щоб екран відображався тільки при першому включанні програми

--- app/src/main/java/com/example/sevenapp/NavigationActivity.java
+++ app/src/main/java/com/example/sevenapp/NavigationActivity.java
@@ -1,66 +1,82 @@
 package com.example.sevenapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.core.text.HtmlCompat;
-import androidx.viewpager.widget.ViewPager;
-
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.text.HtmlCompat;
+import androidx.viewpager.widget.ViewPager;
+
 public class NavigationActivity extends AppCompatActivity {
+
+    private static final String PREF_NAME = "MyPrefs";
+    private static final String FIRST_TIME = "FirstTime";
 
     ViewPager slideViewPager;
     LinearLayout dotIndicator;
     ViewPagerAdapter viewPagerAdapter;
     Button backButton, skipButton, nextButton;
     TextView[] dots;
-    ViewPager.OnPageChangeListener viewPagerListener=new ViewPager.OnPageChangeListener() {
+    ViewPager.OnPageChangeListener viewPagerListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
         }
 
         @Override
         public void onPageSelected(int position) {
             setDotIndicator(position);
-            if(position>0){
+            if (position > 0) {
                 backButton.setVisibility(View.VISIBLE);
             } else {
                 backButton.setVisibility(View.INVISIBLE);
             }
-            if(position==2){
-                nextButton.setText("Finish");
-            }else{
-                nextButton.setText("Next");
+            if (position == 2) {
+                nextButton.setText("Закінчити");
+            } else {
+                nextButton.setText("Наступна");
             }
 
         }
 
         @Override
         public void onPageScrollStateChanged(int state) {
-
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        boolean firstTime = prefs.getBoolean(FIRST_TIME, true);
+
+        if (!firstTime) {
+            Intent intent = new Intent(NavigationActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(FIRST_TIME, false);
+            editor.apply();
+        }
+
         setContentView(R.layout.activity_navigation);
 
-        backButton=findViewById(R.id.backButton);
-        nextButton=findViewById(R.id.nextButton);
-        skipButton=findViewById(R.id.skipButton);
+        backButton = findViewById(R.id.backButton);
+        nextButton = findViewById(R.id.nextButton);
+        skipButton = findViewById(R.id.skipButton);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(getItem(0)>0){
-                    slideViewPager.setCurrentItem(getItem(-1),true);
+                if (getItem(0) > 0) {
+                    slideViewPager.setCurrentItem(getItem(-1), true);
                 }
             }
         });
@@ -68,10 +84,10 @@ public class NavigationActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(getItem(0)<2){
-                    slideViewPager.setCurrentItem(getItem(1),true);
-                }else{
-                    Intent intent=new Intent(NavigationActivity.this,GetStarted.class);
+                if (getItem(0) < 2) {
+                    slideViewPager.setCurrentItem(getItem(1), true);
+                } else {
+                    Intent intent = new Intent(NavigationActivity.this, GetStarted.class);
                     startActivity(intent);
                     finish();
                 }
@@ -81,16 +97,16 @@ public class NavigationActivity extends AppCompatActivity {
         skipButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(NavigationActivity.this, MainActivity.class);
+                Intent intent = new Intent(NavigationActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        slideViewPager=(ViewPager) findViewById(R.id.slideViewPager);
-        dotIndicator=(LinearLayout) findViewById(R.id.dotIndicator);
+        slideViewPager = findViewById(R.id.slideViewPager);
+        dotIndicator = findViewById(R.id.dotIndicator);
 
-        viewPagerAdapter=new ViewPagerAdapter(this);
+        viewPagerAdapter = new ViewPagerAdapter(this);
         slideViewPager.setAdapter(viewPagerAdapter);
 
         setDotIndicator(0);
@@ -110,7 +126,8 @@ public class NavigationActivity extends AppCompatActivity {
         }
         dots[position].setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
     }
-    private int getItem(int i){
-        return slideViewPager.getCurrentItem()+i;
+
+    private int getItem(int i) {
+        return slideViewPager.getCurrentItem() + i;
     }
 }
